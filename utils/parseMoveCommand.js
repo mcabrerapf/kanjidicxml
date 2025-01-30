725def3750ad07cb51df856e1b677c7b141c2398
Manuel Guest <manuelguest@Manuels-MacBook-Pro-2.local>
2025-01-31 00:20:42 +0100

    disabled command parsing

--- utils/parseMoveCommand.js
+++ utils/parseMoveCommand.js
@@ -1,17 +1,18 @@
 const parseMoveCommand = (moveCommand = []) => {
-    return moveCommand
-        .map((currentMove, moveIndex) => {
-            const previousMove = moveCommand[moveIndex - 1];
-            const nextMove = moveCommand[moveIndex + 1];
-            // console.log({previousMove, currentMove, nextMove });
-            if (previousMove === '[+]' || nextMove === '[+]') return null;
-            if (previousMove === 'or' || nextMove === 'or') return null;
-            if (currentMove === '[+]') return `${previousMove}+${nextMove}`;
-            if (currentMove === 'or') return `${previousMove} or ${nextMove}`;
-            return currentMove;
+    return moveCommand;
+    // return moveCommand
+    //     .map((currentMove, moveIndex) => {
+    //         const previousMove = moveCommand[moveIndex - 1];
+    //         const nextMove = moveCommand[moveIndex + 1];
+    //         // console.log({previousMove, currentMove, nextMove });
+    //         if (previousMove === '[+]' || nextMove === '[+]') return null;
+    //         if (previousMove === 'or' || nextMove === 'or') return null;
+    //         if (currentMove === '[+]') return `${previousMove}+${nextMove}`;
+    //         if (currentMove === 'or') return `${previousMove} or ${nextMove}`;
+    //         return currentMove;
 
-        })
-        .filter(Boolean)
+    //     })
+    //     .filter(Boolean)
 }
 
 module.exports = parseMoveCommand;
